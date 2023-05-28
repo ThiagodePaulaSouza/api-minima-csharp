8f8d184233a61b12b7e2c20a03f2e1be43ab051f
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-05-28 16:35:32 -0300

    feat: Adicionado Swagger

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.OpenApi.Models;
+;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Minima", Description = "Testando as parada :D", Version = "v1" }));
 
 builder.Services.AddCors(options => { });
 
 var app = builder.Build();
-
 app.UseCors(Guid.NewGuid().ToString());
 
-app.MapGet("/", () => "api minima csharp :DD");
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Minima"));
+
+app.MapGet("/", () => "api minima csharp :))");
 
 app.Run();
