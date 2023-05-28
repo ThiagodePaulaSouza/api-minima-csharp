e58c43e23521051dc31d93a7edcf859fe7c1caa7
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-05-28 17:48:47 -0300

    feat: Adicionado metodos http Get, Get Id, Post, Put, Delete

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.OpenApi.Models;
 ;
 
@@ -14,6 +15,42 @@ app.UseCors(Guid.NewGuid().ToString());
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Minima"));
 
+string bark()
+{
+    return "huf huf, auau";
+}
+var data = new Dictionary<int, string>() {
+    {1, "salsichinha"},
+    {2, "poodle"},
+    {3, "pincher"},
+    {4, "rottweiler"},
+    {9999, "caramelo"}
+};
+
 app.MapGet("/", () => "api minima csharp :))");
+app.MapGet("/dog", () => bark());
+app.MapGet("/dog/{id}", (int id) => data.SingleOrDefault(dog => dog.Key == id));
+
+List<Book> MeusLivros = new List<Book>();
+
+app.MapGet("/book", () => MeusLivros.Select(o => o));
+app.MapGet("/book/{id}", (int id) => MeusLivros.Single(o => o.id == id));
+app.MapPost("/book", (Book book) => MeusLivros.Add(book));
+app.MapPut("/book/{id}", (int id, Book book) => MeusLivros[id] = book);
+app.MapDelete("/book/{id}", (int id) => MeusLivros.Remove(MeusLivros.Single(o => o.id == id)));
 
 app.Run();
+
+public class Book
+{
+    public Book(int id, string name, decimal price)
+    {
+        this.id = id; ;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int id { get; set; }
+    public string name { get; set; }
+    public decimal price { get; set; }
+}
